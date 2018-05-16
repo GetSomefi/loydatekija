18234f3416ca9145f38a4d3e76d04c4335a3103a
Peter Virtanen <peyte.com@gmail.com>
2018-05-16 12:05:07 +0300

    16.5.2018 parannetut kieliversiot ja banneri

---
+++ wp-content/themes/beat-child/banner1.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * The template for displaying the banner 1
+ */
+?>
+<section class="beat-awesome-banner">
+  <div class="container">
+    <h2 class="h1">ILMAVA<br />BANNERI</h2>
+  </div>
+</section>
